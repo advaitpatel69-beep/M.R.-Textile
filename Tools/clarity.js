9856ae8d02829081fb50204039dec81e07acd839
advaitpatel69-beep <advaitpatel69-beep@users.noreply.github.com>
2026-07-20 18:50:14 +0530

    feat: add Microsoft Clarity integration across all pages and reformat HTML for improved readability

---
+++ Tools/clarity.js
@@ -0,0 +1,22 @@
+// // Load the Google Tag library dynamically
+// var script = document.createElement('script');
+// script.src = 'https://www.googletagmanager.com/gtag/js?id=G-B0M63KX41H';
+// script.async = true;
+// document.head.appendChild(script);
+
+// // Initialize tracking
+// window.dataLayer = window.dataLayer || [];
+// function gtag(){dataLayer.push(arguments);}
+// gtag('js', new Date());
+// gtag('config', 'G-B0M63KX41H');
+
+
+
+// clarity code 
+
+(function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "xpf0iebruz");
+
